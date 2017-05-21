8697e584d100b4535c7c79ed31e1d0969fcb1802
Eric Gauderman <eric.gauderman@gmail.com>
2017-05-21 12:04:35 -0700

    Translated svg coordinates to have origin at center, debugging

--- src/App.elm
+++ src/App.elm
@@ -2,14 +2,16 @@ module App exposing (..)
 
 -- Third-party
 
-import Html as H
-import Html.Attributes as H
-import Mouse
 import Char
 import Debug
+import Html as H
+import Html.Attributes as H
 import Keyboard
+import Mouse
 import Svg as S
 import Svg.Attributes as SA
+import Task
+import Window
 
 
 -- Mine
@@ -20,7 +22,7 @@ import Grid
 
 gridConfig : Grid.GridConfig
 gridConfig =
-    { diameter = 30
+    { diameter = 60
     , shape = Grid.HexPointyTop
     }
 
@@ -57,6 +59,20 @@ naturalColors =
     False
 
 
+viewportToCenteredCoordinates : Window.Size -> Mouse.Position -> Grid.PixelCoords
+viewportToCenteredCoordinates { width, height } { x, y } =
+    Grid.PixelCoords
+        (toFloat x - (toFloat width / 2))
+        (toFloat y - (toFloat height / 2))
+
+
+centeredToViewportCoordinates : Window.Size -> Grid.PixelCoords -> Mouse.Position
+centeredToViewportCoordinates { width, height } { x, y } =
+    Mouse.Position
+        (floor x + (width // 2))
+        (floor y + (height // 2))
+
+
 type alias Spot =
     { gridCoords : Grid.GridCoords
     , index : Int
@@ -65,32 +81,34 @@ type alias Spot =
 
 type alias Model =
     { spots : List Spot
+    , windowSize : Window.Size
     }
 
 
 type Msg
     = MouseMove Mouse.Position
     | KeyPress Keyboard.KeyCode
+    | WindowResize Window.Size
 
 
 init : ( Model, Cmd Msg )
 init =
     { spots = []
+    , windowSize = Window.Size 0 0
     }
-        ! []
+        ! [ Task.perform WindowResize Window.size
+          ]
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     (case msg of
-        MouseMove position ->
+        MouseMove mousePosition ->
             let
                 mouseGridCoords : Grid.GridCoords
                 mouseGridCoords =
                     Grid.pixelToGrid gridConfig
-                        { x = (toFloat position.x)
-                        , y = (toFloat position.y)
-                        }
+                        (viewportToCenteredCoordinates model.windowSize mousePosition)
             in
                 case List.head model.spots of
                     Nothing ->
@@ -112,6 +130,9 @@ update msg model =
 
                 _ ->
                     model ! []
+
+        WindowResize size ->
+            { model | windowSize = size } ! []
     )
         |> limitSpots
 
@@ -126,6 +147,7 @@ subscriptions model =
     Sub.batch
         [ Mouse.moves MouseMove
         , Keyboard.presses KeyPress
+        , Window.resizes WindowResize
         ]
 
 
@@ -135,38 +157,55 @@ view model =
         [ H.div [ H.id "overlay" ]
             [ CreativeCommonsLicense.view [ H.class "license" ]
             ]
-        , S.svg [ SA.id "graphics" ]
-            ((model.spots
-                |> List.indexedMap viewSpot
-                -- Reverse so that new spots show up over old spots:
-                |> List.reverse
-             )
-             --++ ((List.range 0 99)
-             --        |> List.map
-             --            (\i ->
-             --                S.g []
-             --                    -- Vertical line
-             --                    [ S.line
-             --                        [ SA.x1 (toString (toFloat i * gridConfig.diameter / 2))
-             --                        , SA.y1 "0"
-             --                        , SA.x2 (toString (toFloat i * gridConfig.diameter / 2))
-             --                        , SA.y2 "4000"
-             --                        , SA.stroke "white"
-             --                        ]
-             --                        []
-             --                    -- Horizontal line
-             --                    , S.line
-             --                        [ SA.x1 "0"
-             --                        , SA.y1 (toString (toFloat i * gridConfig.diameter / 2 / sqrt 3))
-             --                        , SA.x2 "4000"
-             --                        , SA.y2 (toString (toFloat i * gridConfig.diameter / 2 / sqrt 3))
-             --                        , SA.stroke "white"
-             --                        ]
-             --                        []
-             --                    ]
-             --            )
-             --   )
-            )
+        , H.div [ H.class "svgContainer" ]
+            [ S.svg
+                [ SA.id "graphics"
+                , SA.viewBox <|
+                    String.concat
+                        [ toFloat -model.windowSize.width / 2 |> toString
+                        , " "
+                        , toFloat -model.windowSize.height / 2 |> toString
+                        , " "
+                        , model.windowSize.width |> toString
+                        , " "
+                        , model.windowSize.height |> toString
+                        ]
+                , SA.width (model.windowSize.width |> toString)
+                , SA.height (model.windowSize.height |> toString)
+                ]
+                ((model.spots
+                    |> List.indexedMap viewSpot
+                    -- Reverse so that new spots show up over old spots:
+                    |> List.reverse
+                 )
+                    ++ ((List.range -100 100)
+                            |> List.map
+                                (\i ->
+                                    S.g []
+                                        -- Vertical line
+                                        [ S.line
+                                            [ SA.x1 (toString (toFloat i * gridConfig.diameter / 2))
+                                            , SA.y1 "-4000"
+                                            , SA.x2 (toString (toFloat i * gridConfig.diameter / 2))
+                                            , SA.y2 "4000"
+                                            , SA.stroke "gray"
+                                            ]
+                                            []
+
+                                        -- Horizontal line
+                                        , S.line
+                                            [ SA.x1 "-4000"
+                                            , SA.y1 (toString (toFloat i * gridConfig.diameter / 2 / sqrt 3))
+                                            , SA.x2 "4000"
+                                            , SA.y2 (toString (toFloat i * gridConfig.diameter / 2 / sqrt 3))
+                                            , SA.stroke "gray"
+                                            ]
+                                            []
+                                        ]
+                                )
+                       )
+                )
+            ]
         ]
 
 
